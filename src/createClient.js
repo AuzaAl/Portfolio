eb0789778e31ca496c8e292c434750ed0bccdd76
AuzaAl <auzaganteng007@gmail.com>
2025-05-06 18:54:54 +0700

    Update createClient.js
    change client information

--- src/createClient.js
+++ src/createClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://krwohqgunvfcwkrpwqbd.supabase.co"
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtyd29ocWd1bnZmY3drcnB3cWJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5NjE3MDEsImV4cCI6MjA2MDUzNzcwMX0.RjfUBwjHWTm9OCFXrDfB18yPxHLDNKgY9jI4IxGnu4k'
+const supabaseUrl = "<<YourURL>>"
+const supabaseKey = '<<YourKEY>>'
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
